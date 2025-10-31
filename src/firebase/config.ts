3c663a38e0ca2dff36c8af1e6fb5091a34b70afe
Kissy Carolina Manjarrez Murillo <kissycarolinamanjarresmurillo@gmail.com>
2025-10-31 15:53:31 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1490001049-9f599",
+  "appId": "1:236831116505:web:1ea35286a65d2ac05642c8",
+  "apiKey": "AIzaSyDoyuvFjKHpT-ZXJ1NWqvCe6_AYEr5NvwA",
+  "authDomain": "studio-1490001049-9f599.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "236831116505"
+};
